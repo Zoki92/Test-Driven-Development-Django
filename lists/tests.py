c9ffa632b1eec7d39ae309763e71ad7e95fb5f90
Zoran Stoilov <zoranstoilov@yahoo.com>
2019-02-21 22:00:34 +0000

    Added func tests as app

--- lists/tests.py
+++ lists/tests.py
@@ -17,7 +17,7 @@ class HomePageTest(TestCase):
 		self.assertEqual(Item.objects.count(), 0)
 
 	def test_can_save_a_POST_request(self):
-		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.client.post('/', data={'item_text': 'A new list item'})
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
@@ -26,6 +26,16 @@ class HomePageTest(TestCase):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
 		self.assertEqual(response.status_code, 302)
 		self.assertEqual(response['location'], '/')
+
+	def test_displays_all_list_items(self):
+		Item.objects.create(text="itemey 1")
+		Item.objects.create(text="itemey 2")
+
+		response = self.client.get('/')
+
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
+			
 
 
 class ItemModelTest(TestCase):
@@ -48,14 +58,3 @@ class ItemModelTest(TestCase):
 		self.assertEqual(second_saved_item.text, 'Item the second')
 
 
-class HomePageTest(TestCase):
-
-	def test_displays_all_list_items(self):
-		Item.objects.create(text="itemey 1")
-		Item.objects.create(text="itemey 2")
-
-		response = self.client.get('/')
-
-		self.assertIn('itemey 1', response.content.decode())
-		self.assertIn('itemey 2', response.content.decode())
-		
