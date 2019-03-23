43dc78a3a9f019a37fe1af1380f8ce3387633f81
Zoran Stoilov <zoranstoilov@yahoo.com>
2019-03-23 09:03:57 +0000

    Moved Fts into their own individual files

--- functional_tests/simple_list_creation.py
+++ functional_tests/simple_list_creation.py
@@ -1,53 +1,12 @@
+from .base import FunctionalTest
 from selenium import webdriver
-import unittest
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.keys import Keys
-import time
-from selenium.common.exceptions import WebDriverException
 
-MAX_WAIT = 10
 
-class NewVisitorTest(StaticLiveServerTestCase):
-	def setUp(self):
-		self.browser = webdriver.Firefox()
+class NewVisitorTest(FunctionalTest):
 
-	def wait_for_row_in_list_table(self, row_text):
-		start_time = time.time()
-		while True:
-			try:
-				table = self.browser.find_element_by_id('id_list_table')
-				rows = self.browser.find_elements_by_tag_name('tr')
-				self.assertIn(row_text, [row.text for row in rows])
-				return
-			except (AssertionError, WebDriverException) as e:
-				if time.time() - start_time > MAX_WAIT:
-					raise e
-				time.sleep(0.5)
 
-	def test_layout_and_styling(self):
-		self.browser.get(self.live_server_url)
-		self.browser.set_window_size(1024, 768)
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		"""
-		checks to see that the css we have on inputbox is correct
-		it's been set to be in the middle of the page
-		"""
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width'] / 2,
-			512,
-			delta=10
-			)
-		inputbox.send_keys('testing')
-		inputbox.send_keys(Keys.ENTER)
-		self.wait_for_row_in_list_table('1: testing')
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width'] / 2,
-			512,
-			delta=10
-			)
-
-	def test_can_start_a_list_and_retrieve_it_later(self):
+	def test_can_start_a_list_for_one_user(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
 		self.browser.get(self.live_server_url)
@@ -58,9 +17,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		# She is invited to enter a to-do item straight away
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
-		inputbox.get_attribute('placeholder'),
-		'Enter a to-do item'
-		)
+						inputbox.get_attribute('placeholder'),
+						'Enter a to-do item'
+						)
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is tying fly-fishing lures)
 		inputbox.send_keys('Buy peacock feathers')
@@ -72,15 +31,12 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
+		
 
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
 		
-
-
-		self.fail('Finish the test')
-
 	def test_multiple_users_can_start_lists_at_different_urls(self):
 		self.browser.get(self.live_server_url)
 		inputbox = self.browser.find_element_by_id('id_new_item')
@@ -110,8 +66,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
 
-	def tearDown(self):
-		self.browser.quit()
-
-# if __name__ == '__main__':
-# 	unittest.main(warnings='ignore')
