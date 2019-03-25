dbe6f3d074f6cc24d21a2d09978b9d693222b2c5
Zoran Stoilov <zoranstoilov@yahoo.com>
2019-03-25 17:20:01 +0000

    new duplicate list item form

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -102,6 +102,16 @@ class ListViewTest(TestCase):
 		response = self.post_invalid_input()
 		self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
+	def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+		list1 = List.objects.create()
+		item1 = Item.objects.create(list=list1, text='textey')
+		response = self.client.post(f'/lists/{list1.id}/', data={'text': 'textey'})
+
+		expected_error = escape("You've already got this in your list")
+		self.assertContains(response, expected_error)
+		self.assertTemplateUsed(response, 'list.html')
+		self.assertEqual(Item.objects.count(), 1)
+
 class NewListView(TestCase):
 
 	def test_for_invalid_input_renders_home_template(self):
